b84eb1dddf983a88d703daeca746754818dd1654
jds23288 <jondarsor@gmail.com>
2026-07-18 01:26:08 +0200

    Configuracion inicial: Spring Boot, PostgreSQL y Spring Security

---
+++ src/main/java/com/jondarsor/wedding_planner_backend/WeddingPlannerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jondarsor.wedding_planner_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingPlannerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingPlannerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jondarsor/wedding_planner_backend/WeddingPlannerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jondarsor.wedding_planner_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingPlannerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
